1e1fbc980c658d36957006a51dd50abe7d5b59e5
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2023-04-09 22:25:04 +0900

    Form Login 인증

--- src/main/java/coid/security/springsecurity/controller/HomeController.java
+++ src/main/java/coid/security/springsecurity/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
 	public String home() {
 		return "home";
 	}
+
+	@GetMapping("loginPage")
+	public String loginPage() {
+		return "loginPage";
+	}
 }
--- src/main/java/coid/security/springsecurity/security/SecurityConfig.java
+++ src/main/java/coid/security/springsecurity/security/SecurityConfig.java
@@ -1,10 +1,18 @@
 package coid.security.springsecurity.security;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -16,7 +24,41 @@ public class SecurityConfig {
 			.authorizeRequests()
 			.anyRequest().authenticated();
 		http
-			.formLogin();
+			.formLogin()
+			.loginPage("/loginPage")             //사용자 정의 로그인 페이지
+
+			//region 핸들러없이 바로 이동페이지 설정
+			.defaultSuccessUrl("/")		         // 로그인 성공 후 이동 페이지
+			.failureUrl("/login")   			 // 로그인 실패 후 이동페이지
+			//endregion
+
+			//region 제공해주는 로그인페이지 파라마터 name 및 Action 셋팅
+			.usernameParameter("userId")    	 // 아이디 파라미터명 설정
+			.passwordParameter("passwd")     	 //패스워드 파라미터명 설정
+			.loginProcessingUrl("/login_proc")   //로그인 form Action url 설정
+			//endregion
+
+			// 로그인 성공후 핸들러
+			.successHandler(new AuthenticationSuccessHandler() {
+				@Override
+				public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+					System.out.println("authentication" + authentication.getName());
+					response.sendRedirect("/");
+				}
+			})
+
+
+			// 로그인 실패 후 핸들러
+			.failureHandler(new AuthenticationFailureHandler() {
+				@Override
+				public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+					System.out.println("exception" + exception.getMessage());
+					response.sendRedirect("/login");
+				}
+			})
+
+			//로그인페이지를 모든 사용자가 접근 가능하도록 설정
+			.permitAll();
 
 		return http.build();
 	}
