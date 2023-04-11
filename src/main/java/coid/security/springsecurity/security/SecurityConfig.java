2ea28403bbab5bf487f1e4060a794aeca9dc5986
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2023-04-11 22:09:25 +0900

    Spring Boot 기반으로 개발하는 Spring Security : LogoutFilter

--- src/main/java/coid/security/springsecurity/security/SecurityConfig.java
+++ src/main/java/coid/security/springsecurity/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,8 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -25,16 +28,16 @@ public class SecurityConfig {
 			.anyRequest().authenticated();
 		http
 			.formLogin()
-			.loginPage("/loginPage")             //사용자 정의 로그인 페이지
+			// .loginPage("/login")             //사용자 정의 로그인 페이지
 
 			//region 핸들러없이 바로 이동페이지 설정
-			.defaultSuccessUrl("/")		         // 로그인 성공 후 이동 페이지
-			.failureUrl("/login")   			 // 로그인 실패 후 이동페이지
+			// .defaultSuccessUrl("/")                 // 로그인 성공 후 이동 페이지
+			// .failureUrl("/login")             // 로그인 실패 후 이동페이지
 			//endregion
 
 			//region 제공해주는 로그인페이지 파라마터 name 및 Action 셋팅
-			.usernameParameter("userId")    	 // 아이디 파라미터명 설정
-			.passwordParameter("passwd")     	 //패스워드 파라미터명 설정
+			.usernameParameter("userId")         // 아이디 파라미터명 설정
+			.passwordParameter("passwd")         //패스워드 파라미터명 설정
 			.loginProcessingUrl("/login_proc")   //로그인 form Action url 설정
 			//endregion
 
@@ -47,7 +50,6 @@ public class SecurityConfig {
 				}
 			})
 
-
 			// 로그인 실패 후 핸들러
 			.failureHandler(new AuthenticationFailureHandler() {
 				@Override
@@ -56,10 +58,32 @@ public class SecurityConfig {
 					response.sendRedirect("/login");
 				}
 			})
+			.permitAll()  //로그인페이지를 모든 사용자가 접근 가능하도록 설정
+			.and();
 
-			//로그인페이지를 모든 사용자가 접근 가능하도록 설정
-			.permitAll();
+			//region logout
+		http
+			.logout()                                     //Post로 진행해야함
+			.logoutUrl("/logout")                         // 로그아웃 처리 url
+			.logoutSuccessUrl("/login")                  // 로그아웃 성공 후 이동페이지
+			.addLogoutHandler(new LogoutHandler() {      // 로그아웃 핸들러
+				@Override
+				public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+					HttpSession session = request.getSession();
+					session.invalidate();
+				}
+			})
+			.logoutSuccessHandler(new LogoutSuccessHandler() { 		 // 로그아웃 성공 후 핸들러
+				@Override
+				public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+					response.sendRedirect("/login");
+				}
+			})
+			.deleteCookies("JSESSIONID", "remember-me");  // 로그아웃 후 쿠키 삭제
+			// endregion
 
+
+		;
 		return http.build();
 	}
 
