3f6a725fea89d22bcc320d7ee2802f7e59a01a61
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2023-04-16 19:39:16 +0900

    ExceptionTranslationFilter, RequestCacheAwareFilter

--- src/main/java/coid/security/springsecurity/controller/HomeController.java
+++ src/main/java/coid/security/springsecurity/controller/HomeController.java
@@ -25,4 +25,9 @@ public class HomeController {
 	public String admin() {
 		return "admin";
 	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
 }
--- src/main/java/coid/security/springsecurity/security/SecurityConfig.java
+++ src/main/java/coid/security/springsecurity/security/SecurityConfig.java
@@ -1,21 +1,28 @@
 package coid.security.springsecurity.security;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import javax.sql.DataSource;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 @Configuration
 @EnableWebSecurity
@@ -25,12 +32,37 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
+			.antMatchers("/login").permitAll() 	// login 페이지는 인증이 안돼도 접근이 가능해야하기 때문에
 			.antMatchers("/user").hasRole("USER")
 			.antMatchers("/admin/pay").hasRole("ADMIN")
 			.antMatchers("/admin/**").access("hasRole('ADMIN') or hasAnyRole('sys')")
 			.anyRequest().authenticated();
 		http
-			.formLogin();
+			.formLogin()
+			.successHandler(new AuthenticationSuccessHandler() {
+				@Override
+				public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+					RequestCache requestCache = new HttpSessionRequestCache();
+					SavedRequest savedRequest = requestCache.getRequest(request, response);
+					String redirectUrl = savedRequest.getRedirectUrl();
+					response.sendRedirect(redirectUrl);
+				}
+			});
+
+		http
+			.exceptionHandling()
+			.authenticationEntryPoint(new AuthenticationEntryPoint() {   // 인증에 대한 예외
+				@Override
+				public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+					response.sendRedirect("/login");
+				}
+			})
+			.accessDeniedHandler(new AccessDeniedHandler() {    // 권한에 대한 예외
+				@Override
+				public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+					response.sendRedirect("/denied");
+				}
+			});
 		return http.build();
 	}
 
