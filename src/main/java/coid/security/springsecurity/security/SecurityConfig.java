deb7505a80e154200c9a3df8bd6b38decd51bca2
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2023-04-19 22:08:17 +0900

    CSRF

--- src/main/java/coid/security/springsecurity/security/SecurityConfig.java
+++ src/main/java/coid/security/springsecurity/security/SecurityConfig.java
@@ -36,47 +36,15 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-			.antMatchers("/login").permitAll() 	// login 페이지는 인증이 안돼도 접근이 가능해야하기 때문에
-			.antMatchers("/user").hasRole("USER")
-			.antMatchers("/admin/pay").hasRole("ADMIN")
-			.antMatchers("/admin/**").access("hasRole('ADMIN') or hasAnyRole('sys')")  //표현식을 사용해 두개이상의 권한을 설정
-			.anyRequest().authenticated();
+			.anyRequest()
+			.permitAll();
 
 		http
-			.formLogin()
-				.successHandler(new AuthenticationSuccessHandler() {
-					@Override
-					public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-						RequestCache requestCache = new HttpSessionRequestCache();
-						SavedRequest savedRequest = requestCache.getRequest(request, response);
-						String redirectUrl = savedRequest.getRedirectUrl();
-						response.sendRedirect(redirectUrl);
-					}
-				});
+			.formLogin();
 
-		http.exceptionHandling()
-			// .authenticationEntryPoint(new AuthenticationEntryPoint() {
-			// 	@Override
-			// 	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-			// 		response.sendRedirect("/login");
-			// 	}
-			// })
-			.accessDeniedHandler(new AccessDeniedHandler() {
-				@Override
-				public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-					response.sendRedirect("/denied");
-				}
-			});
+		http.csrf().disable();
 
 		return http.build();
 	}
 
-	@Bean
-	public InMemoryUserDetailsManager userDetailsService() {
-		List<UserDetails> users = new ArrayList<>();
-		users.add(User.builder().username("user").password("{noop}1111").roles("USER").build());
-		users.add(User.builder().username("sys").password("{noop}11111").roles("SYS").build());
-		users.add(User.builder().username("admin").password("{noop}11111").roles("ADMIN", "USER").build());
-		return new InMemoryUserDetailsManager(users);
-	}
 }
