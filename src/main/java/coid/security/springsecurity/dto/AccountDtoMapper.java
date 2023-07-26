379e2a4a1a6e856b9787235be26a99cc4a5673f5
백현명 <81284265+whitewise95@users.noreply.github.com>
2023-07-26 19:38:43 +0900

    5-2) 인증 필터 - AjaxAuthenticationFilter

--- src/main/java/coid/security/springsecurity/dto/AccountDtoMapper.java
+++ src/main/java/coid/security/springsecurity/dto/AccountDtoMapper.java
@@ -1,6 +1,7 @@
 package coid.security.springsecurity.dto;
 
 import coid.security.springsecurity.dmain.Account;
+import java.io.BufferedReader;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -10,4 +11,6 @@ public interface AccountDtoMapper {
 	AccountDtoMapper INSTANCE = Mappers.getMapper(AccountDtoMapper.class);
 
 	Account create(AccountDto accountDto);
+
+	AccountDto login(BufferedReader reader);
 }
