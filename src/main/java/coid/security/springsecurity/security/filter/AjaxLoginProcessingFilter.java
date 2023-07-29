35398a5d3bb828a15150985437a94efce0f96db4
whitewise95 <81284265+whitewise95@users.noreply.github.com>
2023-07-29 18:07:28 +0900

    수정

--- src/main/java/coid/security/springsecurity/security/filter/AjaxLoginProcessingFilter.java
+++ src/main/java/coid/security/springsecurity/security/filter/AjaxLoginProcessingFilter.java
@@ -3,10 +3,13 @@ package coid.security.springsecurity.security.filter;
 import coid.security.springsecurity.dto.AccountDto;
 import coid.security.springsecurity.dto.AccountDtoMapper;
 import coid.security.springsecurity.security.token.AjaxAuthenticationToken;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.BufferedReader;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
@@ -14,6 +17,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.util.StringUtils;
 
 public class AjaxLoginProcessingFilter extends AbstractAuthenticationProcessingFilter {
+
+	@Autowired
+	private ObjectMapper objectMapper;
 
 	public AjaxLoginProcessingFilter() {
 		super(new AntPathRequestMatcher("/api/login"));
@@ -26,7 +32,7 @@ public class AjaxLoginProcessingFilter extends AbstractAuthenticationProcessingF
 			throw new IllegalStateException("Authentication is not supported");
 		}
 
-		AccountDto accountDto = AccountDtoMapper.INSTANCE.login(request.getReader());
+		AccountDto accountDto = objectMapper.readValue(request.getReader(), AccountDto.class);
 		if (StringUtils.isEmpty(accountDto.getUsername()) || StringUtils.isEmpty(accountDto.getPassword())) {
 			throw new IllegalArgumentException("Username or Password is empty");
 		}
